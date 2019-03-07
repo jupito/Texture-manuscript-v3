218e4629067d54bf2eb3fcee9a1f3795698461bd
Jussi Toivonen <jupito@iki.fi>
2019-03-07 23:57:30 +0200

    version with changes highlighted; response edited

---
+++ build_all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#latexmk -pdf main-els
+latexmk -pdf main-plos
+
+latexdiff -e utf8 --flatten main-plos-old.tex main-plos.tex > main-plos-diff.tex
+latexmk -pdf main-plos-old
+latexmk -pdf main-plos-diff
