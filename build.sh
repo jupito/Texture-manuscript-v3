59a6fd57af085b429bc2aaed538ea132e5bbfbb7
Jussi Toivonen <jupito@iki.fi>
2019-05-31 21:34:12 +0300

    build scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #latexmk -pdf main-els
-latexmk -pdf main-plos
+#latexmk -pdf main-plos
+latexmk -pdf main-plos-singlefile
 
 #latexdiff -e utf8 --flatten main-plos-old.tex main-plos.tex > main-plos-diff.tex
 #latexmk -pdf main-plos-old
